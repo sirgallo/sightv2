6cbc11db08b4825abeaeafad6456a43b6dd54226
Sir Gallo <nickgallo97@gmail.com>
2024-05-10 20:48:21 -0400

    minor update

--- backend/db/models/ACL.ts
+++ backend/db/models/ACL.ts
@@ -8,7 +8,7 @@ export type AccessControlLevel = typeof accessControlList[number];
 type Admin = 'ADMIN';
 
 const permissionedUserList = <const>[ // full access, can update permissions on models and on users
-  'ARCHICTECT',  // can create models and data sources. writes to models that they have permissions on (creating a model gives write permission)
+  'ARCHITECT',  // can create models and data sources. writes to models that they have permissions on (creating a model gives write permission)
   'ANALYST' // read only access to models and data sources
 ]
 export type PermissionedUser = typeof permissionedUserList[number];
